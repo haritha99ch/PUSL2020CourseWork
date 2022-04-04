544649dc33a8e69efe3a7ddbe83442333c81dd13
Haritha Rathnayake <chamararathnayaka@outlook.com>
2022-04-04 18:27:07 +0530

    Models: Created and configured Report models

---
+++ AccidentsReports/Models/ReportModels.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AccidentsReports.Models {
+    public class ReportModels {
+    }
+    public abstract class Report {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Location { get; set; }
+        public string Category { get; set; }
+        public string Description { get; set; }
+        public float DamageScale { get; set; }
+        public string ImagePath { get; set; }
+    }
+    public class ReportDetail:Report {
+        public string Cause { get; set; }
+        public string ApprovedBy { get; set; }
+        public string DamageEstimatedBy { get; set; }
+        public string ClaimedBy { get; set; }
+        public List<string> ImagePaths { get; set; }
+    }
+}
