4df7557f98da9742b087fbf512462802b18fa294
Haritha Rathnayake <chamararathnayaka@outlook.com>
2022-04-05 17:09:08 +0530

    Views: Created simple Login view

---
+++ AccidentsReports/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AccidentsReports.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string Email, string Password)
+        {
+            if(!(string.IsNullOrEmpty(Email) || string.IsNullOrEmpty(Password))) {
+                ViewBag.Email = Email;
+                ViewBag.Password = Password;
+            }
+            return View();
+        }
+    }
+}
