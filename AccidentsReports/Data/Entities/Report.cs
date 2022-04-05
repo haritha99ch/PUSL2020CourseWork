0cacb9bc8eaa1edfbd4945b493ea15f5c477b339
Haritha Rathnayake <chamararathnayaka@outlook.com>
2022-04-05 16:19:33 +0530

    BugFix: Fixed a bug in EF Migration stage

--- AccidentsReports/Data/Entities/Report.cs
+++ AccidentsReports/Data/Entities/Report.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
@@ -10,20 +12,27 @@ namespace AccidentsReports.Data.Entities {
         public int Id { get; set; }
         [ForeignKey("Driver")]
         public int Author { get; set; }
+        [Required]
         public string Title { get; set; }
+        [Required]
         public string Description { get; set; }
+        [Required]
         public float Latitude { get; set; }
-        public float Altitude { get; set; }
+        [Required]
+        public float Longitude { get; set; }
+        [Required]
         public string Category { get; set; }
         [ForeignKey("Police")]
-        public int ApprovedBy { get; set; }
+        public int? ApprovedBy { get; set; }
         [ForeignKey("RDA")]
-        public int EstimatedBy { get; set; }
+        public int? EstimatedBy { get; set; }
         [ForeignKey("Insurance")]
-        public int ClaimedBy { get; set; }
-        public float DamageScale { get; set; }
-        public float Damage { get; set; }
+        public int? ClaimedBy { get; set; }
+        public float? DamageScale { get; set; }
+        public float? Damage { get; set; }
+        [Required]
         public ICollection<Vehicle> Vehicles { get; set; }
+        [Required]
         public ICollection<Image> Images { get; set; }
         public virtual Driver Driver { get; set; }
         public virtual Police Police { get; set; }
