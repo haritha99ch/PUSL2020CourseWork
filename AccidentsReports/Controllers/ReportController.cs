acee2b9953a8385debc1646e7571d27cf6d929ed
Haritha Rathnayake <chamararathnayaka@outlook.com>
2022-04-09 17:54:46 +0530

    Controller: Created New Report Request

---
+++ AccidentsReports/Controllers/ReportController.cs
@@ -0,0 +1,67 @@
+﻿using AccidentsReports.Data;
+using AccidentsReports.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AccidentsReports.Controllers {
+    public class ReportController : Controller {
+        long currentUser;
+        // GET: Report
+        public ActionResult Index() {
+            currentUser = (long)Session["CurrentUserID"];
+            return View();
+        }
+        public ActionResult Create() {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(ReportDetail request) {
+            using (var db = new ARDbContext()) {
+
+                var vehicles = new List<Data.Entities.Vehicle>();
+                foreach (var vehicle in request.Vehicles) {
+                    vehicles.Add(new Data.Entities.Vehicle() {
+                        DriverLicence = vehicle.LicenceNumber,
+                        Class = vehicle.Class,
+                        ModelName = vehicle.ModelName
+                    });
+                }
+
+                var images = new List<Data.Entities.Image>();
+                int count=0;
+                foreach (var Image in request.ImagePaths) {
+                    images.Add(new Data.Entities.Image() {
+                        ImagePath = $"~/Content/images/reports/{currentUser}-{DateTime.Now.Date}-{count}.png"
+                    });
+                    count++;
+                }
+
+                var newReport = new Data.Entities.Report() {
+                    AuthorLicence = db.Drivers.FirstOrDefault(d => d.DriverNIC.Equals(currentUser)).LicenceId,
+                    Vehicles = vehicles,
+                    Images= images
+                };
+
+                var reportMeta = new Data.Entities.ReportMeta() {
+                    Title = request.Title,
+                    Cause = request.Cause,
+                    DateTime = DateTime.Now,
+                    Description = request.Description,
+                    IsVehicleVehicle = request.IsVehicleVehicle,
+                    IsVehicleProperty = request.IsVehicleProperty,
+                    IsVehiclePedestrian = request.IsVehiclePedestrian,
+                    Latitude = request.Latitude,
+                    Longitude = request.Longitude,
+                    Scale = request.Scale,
+                    Report = newReport
+                };
+
+            }
+            return View();
+        }
+    }
+}
--- AccidentsReports/Models/ReportModels.cs
+++ AccidentsReports/Models/ReportModels.cs
@@ -1,28 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace AccidentsReports.Models {
     public abstract class Report {
         public int Id { get; set; }
+        [Required]
         public string Title { get; set; }
-        public string Author { get; set; }
+        [Required]
+        public long Author { get; set; }
+        [Required]
         public DateTime DatetTime { get; set; }
-        public string Location { get; set; }
+        [Required]
+        public float Latitude { get; set; }
+        [Required]
+        public float Longitude { get; set; }
+        [Required]
         public string Category { get; set; }
+        [Required]
         public string Cause { get; set; }
+        [Required]
         public List<string> VehicleClasses { get; set; }
+        [Required]
         public string Description { get; set; }
-        public float DamageScale { get; set; }
+        [Required]
+        public int Scale { get; set; }
         public string ImagePath { get; set; }
         public string Status { get; set; }
     }
     public class ReportDetail:Report {
         public List<Vehicle> Vehicles { get; set; }
-        public string ApprovedBy { get; set; }
-        public string DamageEstimatedBy { get; set; }
-        public string ClaimedBy { get; set; }
+        [Required]
+        public bool IsVehicleVehicle { get; set; }
+        [Required]
+        public bool IsVehicleProperty { get; set; }
+        [Required]
+        public bool IsVehiclePedestrian { get; set; }
+        [Required]
+        public float Damage { get; set; }
+        public long ApprovedBy { get; set; }
+        public long DamageEstimatedBy { get; set; }
+        public long ClaimedBy { get; set; }
         public List<string> ImagePaths { get; set; }
     }
 }
