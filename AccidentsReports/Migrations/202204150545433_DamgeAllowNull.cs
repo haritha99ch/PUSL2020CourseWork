8cb9b9f448197cb31e67f5d269d188388b967b53
Haritha Rathnayake <chamararathnayaka@outlook.com>
2022-04-15 13:13:32 +0530

    VC: Created all the moderation views and locgics

---
+++ AccidentsReports/Migrations/202204150545433_DamgeAllowNull.cs
@@ -0,0 +1,18 @@
+﻿namespace AccidentsReports.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class DamgeAllowNull : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Reports", "Damage", c => c.Single());
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Reports", "Damage", c => c.Single(nullable: false));
+        }
+    }
+}
